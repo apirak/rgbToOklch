3558b54cc36cefed49cdac0c27bd4e6c469f830b
Apirak Panatkool <apirakb@gmail.com>
2024-05-12 19:45:43 +0700

    finish test rgb2srgbliniear

--- oklch.ts
+++ oklch.ts
@@ -5,6 +5,48 @@ export type RGB = {
   b: number;
 };
 
+export type RGBColor = {
+  r: number;
+  g: number;
+  b: number;
+};
+
+export type sRGBLinearColor = {
+  r: number;
+  g: number;
+  b: number;
+};
+
+// const rgb2srgbLinear = (rgb: Vector3): Vector3 =>
+//   rgb.map((c) =>
+//     Math.abs(c) <= 0.04045
+//       ? c / 12.92
+//       : Math.sign(c) * ((Math.abs(c) + 0.055) / 1.055) ** 2.4
+//   ) as Vector3;
+
+// const rgb2srgbLinear = (rgb: Vector3): Vector3 =>
+//   rgb.map((c) =>
+//     Math.abs(c) <= 0.04045
+//       ? c / 12.92
+//       : (c < 0 ? -1 : 1) * ((Math.abs(c) + 0.055) / 1.055) ** 2.4
+//   ) as Vector3;
+
+export function rgb2srgbLinear(rgb: RGBColor): sRGBLinearColor {
+  const linearize = (channel: number): number => {
+    if (channel <= 0.04045) {
+      return channel / 12.92;
+    } else {
+      return ((channel + 0.055) / 1.055) ** 2.4;
+    }
+  };
+
+  return {
+    r: linearize(rgb.r),
+    g: linearize(rgb.g),
+    b: linearize(rgb.b),
+  };
+}
+
 const multiplyMatrices = (A: number[], B: Vector3): Vector3 => {
   return [
     A[0] * B[0] + A[1] * B[1] + A[2] * B[2],
@@ -27,39 +69,6 @@ const oklab2oklch = ([l, a, b]: Vector3): Vector3 => [
     : ((((Math.atan2(b, a) * 180) / Math.PI) % 360) + 360) % 360,
 ];
 
-const rgb2srgbLinear = (rgb: Vector3): Vector3 =>
-  rgb.map((c) =>
-    Math.abs(c) <= 0.04045
-      ? c / 12.92
-      : (c < 0 ? -1 : 1) * ((Math.abs(c) + 0.055) / 1.055) ** 2.4
-  ) as Vector3;
-
-const srgbLinear2rgb = (rgb: Vector3): Vector3 =>
-  rgb.map((c) =>
-    Math.abs(c) > 0.0031308
-      ? (c < 0 ? -1 : 1) * (1.055 * Math.abs(c) ** (1 / 2.4) - 0.055)
-      : 12.92 * c
-  ) as Vector3;
-
-const oklab2xyz = (lab: Vector3): Vector3 => {
-  const LMSg = multiplyMatrices(
-    [
-      1, 0.3963377773761749, 0.2158037573099136, 1, -0.1055613458156586,
-      -0.0638541728258133, 1, -0.0894841775298119, -1.2914855480194092,
-    ],
-    lab
-  );
-  const LMS = LMSg.map((val) => val ** 3) as Vector3;
-  return multiplyMatrices(
-    [
-      1.2268798758459243, -0.5578149944602171, 0.2813910456659647,
-      -0.0405757452148008, 1.112286803280317, -0.0717110580655164,
-      -0.0763729366746601, -0.4214933324022432, 1.5869240198367816,
-    ],
-    LMS
-  );
-};
-
 const xyz2oklab = (xyz: Vector3): Vector3 => {
   const LMS = multiplyMatrices(
     [
@@ -77,17 +86,6 @@ const xyz2oklab = (xyz: Vector3): Vector3 => {
       0.0259040424655478, 0.7827717124575296, -0.8086757549230774,
     ],
     LMSg
-  );
-};
-
-const xyz2rgbLinear = (xyz: Vector3): Vector3 => {
-  return multiplyMatrices(
-    [
-      3.2409699419045226, -1.537383177570094, -0.4986107602930034,
-      -0.9692436362808796, 1.8759675015077202, 0.04155505740717559,
-      0.05563007969699366, -0.20397695888897652, 1.0569715142428786,
-    ],
-    xyz
   );
 };
 
@@ -115,15 +113,6 @@ function formatNumber(num: number, digits = 2): string {
     return num.toFixed(digits); // Format the number to two decimal places and it's already a string
   }
 }
-
-const oklch2rgb = (lch: Vector3): Vector3 => {
-  const oklab = oklch2oklab(lch);
-  const xyz = oklab2xyz(oklab);
-  const rgbLinear = xyz2rgbLinear(xyz);
-  const rgb = srgbLinear2rgb(rgbLinear);
-
-  return rgb;
-};
 
 const rgb2oklch = (rgb: Vector3): Vector3 => {
   const rgbLinear = rgb2srgbLinear(rgb);
---
+++ rgb2srgbLinear.test.ts
@@ -0,0 +1,44 @@
+import { rgb2srgbLinear, RGBColor, sRGBLinearColor } from './oklch';
+
+describe('convert RGB to OKLCH', () => {
+  it('convert red color', () => {
+    const rgb = { r: 255 / 255, g: 0, b: 0 }; // Red color
+    const result: sRGBLinearColor = rgb2srgbLinear(rgb);
+    expect(result.r).toBeCloseTo(1, 4);
+    expect(result.g).toBeCloseTo(0, 4);
+    expect(result.b).toBeCloseTo(0, 4);
+  });
+
+  it('convert black color', () => {
+    const rgb = { r: 0, g: 0, b: 0 };
+    const result: sRGBLinearColor = rgb2srgbLinear(rgb);
+    expect(result.r).toBeCloseTo(0, 4);
+    expect(result.g).toBeCloseTo(0, 4);
+    expect(result.b).toBeCloseTo(0, 4);
+  });
+
+  it('convert rgba(0, 100, 170, 0.75)', () => {
+    const rgb = { r: 0, g: 100 / 255, b: 170 / 255 }; // Specific color
+    const result: sRGBLinearColor = rgb2srgbLinear(rgb);
+    expect(result.r).toBeCloseTo(0, 4);
+    expect(result.g).toBeCloseTo(0.12743768043564743, 4);
+    expect(result.b).toBeCloseTo(0.4019777798322, 4);
+  });
+
+  it('convert rgba(100, 110, 160, 0.25)', () => {
+    const rgb = { r: 100 / 255, g: 110 / 255, b: 160 / 255 }; // Specific color
+    const result: sRGBLinearColor = rgb2srgbLinear(rgb);
+    expect(result.r).toBeCloseTo(0.127438, 4);
+    expect(result.g).toBeCloseTo(0.155926, 4);
+    expect(result.b).toBeCloseTo(0.351533, 4);
+  });
+
+  it('convert rgba(194, 150, 71)', () => {
+    const rgb = { r: 194 / 255, g: 150 / 255, b: 71 / 255 }; // Specific color
+    // const rgb = { r: 194, g: 150, b: 71 }; // Specific color
+    const result: sRGBLinearColor = rgb2srgbLinear(rgb);
+    expect(result.r).toBeCloseTo(0.539479, 4);
+    expect(result.g).toBeCloseTo(0.304987, 4);
+    expect(result.b).toBeCloseTo(0.06301, 4);
+  });
+});
