c7e53afe2cee72490fe32dbe093b5eb146385c9c
Apirak Panatkool <apirakb@gmail.com>
2024-05-12 09:49:43 +0700

    First commit
    Add gitignore and make jest work

---
+++ oklch.test.ts
@@ -0,0 +1,5 @@
+import { sum } from './oklch';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
---
+++ oklch.ts
@@ -0,0 +1,10 @@
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+// Exporting the functions correctly
+export { sum, subtract };
